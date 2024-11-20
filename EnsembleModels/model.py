95f601221ff0b5f18abc7becfc78a92bac863a30
Your Name <you@example.com>
2024-11-19 21:05:27 -0500

    bug fix

--- EnsembleModels/model.py
+++ EnsembleModels/model.py
@@ -7,7 +7,7 @@ def get_model(name, num_classes):
     if name == 'alexnet':
         model = models.alexnet(pretrained=True)
         # modify the last layer to have the number of classes we need
-        model.classifier[6] == nn.Linear(model.classifier[6].in_features, num_classes) 
+        model.classifier[6] = nn.Linear(model.classifier[6].in_features, num_classes) 
     elif name == 'resnet18':
         model = models.resnet18(pretrained=True)
         #resnet18 has a fc layer as the last layer
@@ -15,7 +15,7 @@ def get_model(name, num_classes):
     elif name == 'vgg16':
         model = models.vgg16(pretrained=True)
         #dedicated classifier layer, same as alexnet
-        model.classifier[6] == nn.Linear(model.classifier[6].in_features, num_classes)
+        model.classifier[6] = nn.Linear(model.classifier[6].in_features, num_classes)
     else:
         raise ValueError("Model not supported")
     return model
